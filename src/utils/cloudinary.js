f00931970097b0207eafcb0af232186320f1c631
ahmedkhaled250 <ahmedkhaled56745@gmail.com>
2024-05-09 04:07:43 +0300

    initial commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -12,3 +12,27 @@ cloudinary.config({
 });
 
 export default cloudinary;
+
+
+
+
+
+
+
+
+
+// import cloudinary from 'cloudinary'
+// import dotenv from "dotenv";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// dotenv.config({ path: path.join(__dirname, "../../config/.env") });
+
+// cloudinary.v2.config({
+//   cloud_name: process.env.cloud_name,
+//   api_key: process.env.cloud_key,
+//   api_secret: process.env.cloud_secret,
+//   secure: true
+// });
+
+// export default cloudinary.v2
